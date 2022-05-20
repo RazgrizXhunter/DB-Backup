4fc19fa230a95a65f38de92bf8cb4cd0e4c72ed5
RazgrizXhunter <algontama@gmail.com>
2022-05-20 17:21:09 -0400

    Add S3 Upload
    Added S3 file upload with Boto3

--- aws.py
+++ aws.py
@@ -5,21 +5,36 @@ from filemanager import file_manager as fmanager
 logger = logging.getLogger("logger")
 
 class aws():
-	s3 = None
+	# s3 = None
 
-	def __init__(self):
+	# def __init__(self):
+	
+	def init_s3(self, credentials: dict) -> bool:
 		try:
+			self.session = boto3.session(
+				aws_access_key_id=credentials["key"],
+				aws_secret_access_key=credentials["secret"]
+			)
 			self.s3 = boto3.resource("s3")
+
+			return True
 		except Exception as e:
 			logger.error(e)
 			sys.exit()
 	
-	def upload(self, file_path, bucket_name):
+	def s3_upload(self, file_path: str, bucket_name: str, file_name: str = "") -> bool:
+		logger.info(f"Preparing to upload file to S3 bucket: {bucket_name}")
+
+		if (not self.s3):
+			logger.error("S3 session uninitialized")
+
 		if (not fmanager.exists(file_path)):
-			logger.warning(f"Failed to upload file {file_path}. The file doesn't exist")
+			logger.error(f"Failed to upload file {file_path}. The file doesn't exist")
+			return False
 		
-		with open(file_path) as file:
-			try:
-				self.s3.bucket(bucket_name).put_object(fmanager.get_name(file_path), file)
-			except Exception as e:
-				logger.error(e)
+		if (not file_name): file_name = fmanager.get_name(file_path)
+		
+		try:
+			self.s3.bucket(bucket_name).upload_file(file_path, fmanager.get_name(file_path))
+		except Exception as e:
+			logger.error(e)
