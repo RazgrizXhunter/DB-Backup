666d0c51534aa184fde41aa4e3f811c6b3dc5e7b
RazgrizXhunter <algontama@gmail.com>
2022-05-23 12:06:26 -0400

    Add helper
    Added function to display
    file size in a more human friendly
    way

--- filemanager.py
+++ filemanager.py
@@ -1,4 +1,4 @@
-import os, time, psutil, logging
+import os, time, psutil, logging, math
 import gzip, zipfile, tarfile
 
 logger = logging.getLogger("logger")
@@ -56,31 +56,42 @@ class file_manager:
 			except Exception as e:
 				logger.error(e)
 		
-		return self.get_directory(absolute_file_path) + zip_filename
+		return zip_filename
 	
 	def created_on(self, file_path):
 		return time.ctime(os.path.getmtime(file_path))
 	
 	def get_free_disk_space(self):
-		return round(psutil.disk_usage("/").free / (2 ** 30), 3)
+		return psutil.disk_usage("/").free
 	
 	def exists(self, path):
 		return os.path.exists(path)
 
-	def get_size(self, path):		
-		size = 0
+	# def get_size(self, path):
+	# 	size = 0
 
-		with os.scandir(path) as sd:
-			for entry in sd:
-				if entry.is_file():
-					size += entry.stat().st_size
-				elif entry.is_dir():
-					size += self.get_size(entry.path)
+	# 	with os.scandir(path) as sd:
+	# 		for entry in sd:
+	# 			if entry.is_file():
+	# 				size += entry.stat().st_size
+	# 			elif entry.is_dir():
+	# 				size += self.get_size(entry.path)
 		
-		return round(size / (2 ** 30), 3)
+	#	return round(size / (2 ** 30), 3)
  
 	def get_name(self, path):
 		return os.path.basename(path)
 	
 	def get_directory(self, path):
 		return os.path.dirname(path)
+	
+	def to_human_readable_size(self, size_bytes):
+		if size_bytes == 0: return "0B"
+		
+		size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+
+		i = int(math.floor(math.log(size_bytes, 1024)))
+
+		p = math.pow(1024, i)
+		s = round(size_bytes / p, 2)
+		return "%s %s" % (s, size_name[i])
