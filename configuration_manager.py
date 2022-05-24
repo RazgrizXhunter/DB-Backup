720e752b668a4755b4f32579f8a8c1652c2a5735
Aldo Tapia <atapia@innovaweb.cl>
2022-05-24 18:16:39 -0400

    Move method
    Method load_config had a more flexible behavior, not only for the database.
    
    Moved to it's own class to preserve Behavioral Programming.

---
+++ configuration_manager.py
@@ -0,0 +1,19 @@
+import os, sys, logging, yaml
+
+logger = logging.getLogger("logger")
+
+class Configuration_manager:
+
+	def __init__(self, filename: str):
+		project_dir = os.path.realpath(os.path.dirname(__file__))
+		config_file_path = os.path.join(project_dir, filename)
+
+		logger.info(f"Opening config file in: {config_file_path}")
+
+		with open(config_file_path) as f:
+			try:
+				self.config = yaml.safe_load(f)
+				logger.info("Loaded")
+			except yaml.YAMLError as error:
+				logger.error(format(error))
+				sys.exit()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import logging
-from backer import S3_backer
+from database_manager import Database_manager
+from configuration_manager import Configuration_manager
 from file_manager import File_manager
 from argument_manager import Argument_manager
 from aws import AWS
@@ -11,11 +12,12 @@ logger = logging.getLogger("logger")
 
 if (__name__ == "__main__"):
 	argmanager = Argument_manager()
-	fmanager = File_manager()
-	backer = S3_backer()
-	backer.load_config("config.yaml")
+	filemanager = File_manager()
+	confmanager = Configuration_manager("config.yaml")
+	dbmanager = Database_manager(confmanager.config)
+	aws = AWS()
 
-	mailer = Mailer(backer.config["sendgrid"]["api_key"])
+	for project in confmanager.config["projects"]:
 
 	aws = AWS()
 
@@ -28,17 +30,17 @@ if (__name__ == "__main__"):
 			schema = project["schema"]
 			logger.debug(f"Now trying to backup schema \"{schema['name']}\"")
 
-			free_disk_space = fmanager.get_free_disk_space()
-			schema_size = backer.get_schema_size(schema["name"])
-			logger.info(f"Free space: {fmanager.to_human_readable_size(free_disk_space)}, Schema size: {fmanager.to_human_readable_size(schema_size)}")
+			free_disk_space = filemanager.get_free_disk_space()
+			schema_size = dbmanager.get_schema_size(schema["name"])
+			logger.info(f"Free space: {filemanager.to_human_readable_size(free_disk_space)}, Schema size: {filemanager.to_human_readable_size(schema_size)}")
 
 			if (free_disk_space < schema_size):
 				logger.critical("Not enough space to back up")
 				continue
 			
-			backup_file_path = backer.dump(schema["name"])
-			compressed_backup_file_path = fmanager.compress(file_path=backup_file_path, remove_original=True)
+			backup_file_path = dbmanager.dump(schema["name"])
+			compressed_backup_file_path = filemanager.compress(file_path=backup_file_path, remove_original=True)
 
-			aws.init_s3(backer.config["bucket"]["aws_access_key_id"], backer.config["bucket"]["aws_secret_access_key"], backer.config["bucket"]["name"])
+			aws.init_s3(confmanager.config["bucket"]["aws_access_key_id"], confmanager.config["bucket"]["aws_secret_access_key"], confmanager.config["bucket"]["name"])
 			
 			aws.s3_upload(compressed_backup_file_path)
