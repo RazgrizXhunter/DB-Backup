407a615a72a29a0689c46ac4cc2696d81458ea2f
Aldo Tapia <atapia@innovaweb.cl>
2022-05-24 15:39:16 -0400

    Add mailing controller

---
+++ mailing.py
@@ -0,0 +1,33 @@
+# using SendGrid"s Python Library
+# https://github.com/sendgrid/sendgrid-python
+import logging
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+logger = logging.getLogger("logger")
+
+class Mailer:
+	api_key = None
+	client = None
+
+	def __init__(self, api_key: str):
+		self.client = SendGridAPIClient(api_key)
+	
+	def compose(self, mail: dict):
+		self.mail = Mail(
+			from_email=mail["from"],
+			to_emails=mail["to"],
+			subject=mail["subject"],
+			html_content=mail["contents"]
+		)
+	
+	def send(self) -> bool:
+		if (not self.mail or not self.client): return False
+
+		try:
+			response = self.client.send(self.mail)
+		except Exception as e:
+			logger.error(f"Could not send email.\n{e}")
+			return False
+		
+		return response.status_code
