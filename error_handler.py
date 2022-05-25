2858fbea0f9e60bb7de20eab431e62c8355b83d2
Aldo Tapia <atapia@innovaweb.cl>
2022-05-25 17:52:08 -0400

    Add error callback

--- error_handler.py
+++ error_handler.py
@@ -1,21 +1,59 @@
-import logging, sys
+import logging, sys, os, atexit, codecs, socket, glob, time, locale
+from mailing import Mailer
+from configuration_manager import Configuration_manager
+
+logger = logging.getLogger("logger")
 
 class Error_handler(logging.Handler):
+	has_failed = False
+
+	def __init__(self) -> None:
+		super().__init__()
+		atexit.register(self.wrapup)
+
 	def emit(self, record):
 		if (record.levelno >= 40):
-			# Send e-mail
-			print("email sent")
+			self.has_failed = True
+	
+	def wrapup(self):
+		confmanager = Configuration_manager()
+		
+		if (not confmanager.config):
+			logger.error("Configuration file has not been loaded properly")
 			sys.exit()
+		
+		if (self.has_failed):
+			mailer = Mailer(confmanager.config["sendgrid"]["api_key"])
+			recipients = confmanager.get_recipients()
 
-# mailer = Mailer(confmanager.config["sendgrid"]["api_key"])
-# recipients = toListOfTuples(confmanager.config["responsibles"])
+			locale.setlocale(locale.LC_TIME, "es_CL")
+			date = time.strftime("%a, %d %b %Y %H:%M:%S")
 
-# mail = {
-# 	"from": "algontama@gmail.com",
-# 	"to": recipients,
-# 	"subject": "Sending with Twilio SendGrid is Fun",
-# 	"contents": "<strong>and easy to do anywhere, even with Python</strong>"
-# }
-# mailer.compose(mail)
+			email = codecs.open("./email/email_body_v3.html", "r")
+			contents = email.read()
+			email.close()
+			
+			log_file = open("./backup.log.tmp", "r")
+			logs = log_file.read()
+			log_file.close()
 
-# mailer.send()
+			contents = contents.replace("{server_name}", socket.gethostname())
+			contents = contents.replace("{log}", logs)
+			contents = contents.replace("{date}", date)
+
+			mail = {
+				"from": confmanager.config["sendgrid"]["sender"],
+				"to": recipients,
+				"subject": "Backup needs attention",
+				"contents": contents
+			}
+
+			mailer.compose(mail)
+			mailer.send()
+			
+			self.cleanup()
+			print("exiting")
+	
+	def cleanup(self):
+		for file in glob.glob("*.tmp"):
+			os.remove(file)
