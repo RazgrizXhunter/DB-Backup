58c4814494968689b7472ef633c5e82b7cd8ef67
Aldo Tapia <atapia@innovaweb.cl>
2022-05-25 17:54:09 -0400

    Add site backup
    Change in logic to do a backup of the site files and the database at the same time.

--- main.py
+++ main.py
@@ -10,21 +10,46 @@ from aws import AWS
 logger = logging.getLogger("logger")
 
 if (__name__ == "__main__"):
+	confmanager = Configuration_manager()
+	confmanager.load_config("config.yaml")
+
 	argmanager = Argument_manager()
 	filemanager = File_manager()
-	confmanager = Configuration_manager("config.yaml")
 	dbmanager = Database_manager(confmanager.config)
+
 	aws = AWS()
+	bucket = confmanager.config["bucket"]
+	
+	aws.init_s3(bucket["aws_access_key_id"], bucket["aws_secret_access_key"], bucket["name"])
 
 	for project in confmanager.config["projects"]:
 
 		if "site" in project:
-			# backup site directory
-			pass
+			site_files = project["site"]
+			logger.info(f"Now trying to backup files in \"{site_files['path']}\"")
+
+			if (not filemanager.exists(site_files["path"])):
+				logger.error("Provided file path doesn't exist")
+				continue
+			
+			if (site_files["compress"]):
+				free_disk_space = filemanager.get_free_disk_space()
+				directory_size = filemanager.get_size(site_files["path"])
+				logger.info(f"Free space: {filemanager.to_human_readable_size(free_disk_space)}, Site directory size: {filemanager.to_human_readable_size(directory_size)}")
+
+				if (free_disk_space < directory_size):
+					logger.critical("Not enough space to backup site files")
+					continue
+
+				compressed_site_backup_file_path = filemanager.compress(file_path=site_files["path"], remove_original=(not site_files["preserve"]))
+			
+				aws.s3_upload(compressed_site_backup_file_path)
+			else:
+				aws.s3_upload(site_files["path"])
 
 		if "schema" in project:
 			schema = project["schema"]
-			logger.debug(f"Now trying to backup schema \"{schema['name']}\"")
+			logger.info(f"Now trying to backup schema \"{schema['name']}\"")
 
 			free_disk_space = filemanager.get_free_disk_space()
 			schema_size = dbmanager.get_schema_size(schema["name"])
@@ -35,8 +60,8 @@ if (__name__ == "__main__"):
 				continue
 			
 			backup_file_path = dbmanager.dump(schema["name"])
-			compressed_backup_file_path = filemanager.compress(file_path=backup_file_path, remove_original=True)
 
-			aws.init_s3(confmanager.config["bucket"]["aws_access_key_id"], confmanager.config["bucket"]["aws_secret_access_key"], confmanager.config["bucket"]["name"])
+			if (schema["compress"]):
+				backup_file_path = filemanager.compress(file_path=backup_file_path, remove_original=(not schema["preserve"]))
 			
-			aws.s3_upload(compressed_backup_file_path)
+			aws.s3_upload(backup_file_path)
