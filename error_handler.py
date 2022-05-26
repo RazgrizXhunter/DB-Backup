1dd68b08c23d86fcf98eaa1a1dd546f778ed2af8
Aldo Tapia <atapia@innovaweb.cl>
2022-05-26 12:07:42 -0400

    Move functions

--- error_handler.py
+++ error_handler.py
@@ -21,45 +21,49 @@ class Error_handler(logging.Handler):
 	def wrapup(self):
 		logger.debug("Wrapping up")
 		
+		if (self.has_failed):
+			self.send_error_mail()
+			
+		self.cleanup()
+	
+	def cleanup(self):
+		logger.debug("Removing temporary files")
+		for file in glob.glob("*.tmp"):
+			os.remove(file)
+	
+	def send_error_mail(self):
+		logger.debug("Sending notification email")
 		confmanager = Configuration_manager()
 		
 		if (not confmanager.config):
 			logger.critical("Configuration file has not been loaded properly")
 		
-		if (self.has_failed):
-			mailer = Mailer(confmanager.config["sendgrid"]["api_key"])
-			recipients = confmanager.get_recipients()
+		mailer = Mailer(confmanager.config["sendgrid"]["api_key"])
+		recipients = confmanager.get_recipients()
 
-			locale.setlocale(locale.LC_TIME, "es_CL")
-			date = time.strftime("%a, %d %b %Y %H:%M:%S")
+		locale.setlocale(locale.LC_TIME, "es_CL.utf8")
+		date = time.strftime("%a, %d %b %Y %H:%M:%S")
 
-			email = codecs.open("./email/email_body.html", "r")
-			contents = email.read()
-			email.close()
-			
-			log_file = open("./backup.log.tmp", "r")
-			logs = log_file.read()
-			log_file.close()
+		email = codecs.open("./email/email_body.html", "r")
+		contents = email.read()
+		email.close()
+		
+		log_file = open("./backup.log.tmp", "r")
+		logs = log_file.read()
+		log_file.close()
 
-			contents = contents.replace("{server_name}", socket.gethostname())
-			contents = contents.replace("{log}", logs)
-			contents = contents.replace("{date}", date)
+		contents = contents.replace("{server_name}", socket.gethostname())
+		contents = contents.replace("{log}", logs)
+		contents = contents.replace("{date}", date)
 
-			mail = {
-				"from": confmanager.config["sendgrid"]["sender"],
-				"to": recipients,
-				"subject": "Backup needs attention",
-				"contents": contents
-			}
+		mail = {
+			"from": confmanager.config["sendgrid"]["sender"],
+			"to": recipients,
+			"subject": "Backup needs attention",
+			"contents": contents
+		}
 
-			mailer.compose(mail)
+		mailer.compose(mail)
 
-			# logger.debug(f"Mail was to be sent with:\n\t{logs}") # We don't wanna fill everyone's inbox with junk
-			# mailer.send()
-			
-			self.cleanup()
-			print("exiting")
-	
-	def cleanup(self):
-		for file in glob.glob("*.tmp"):
-			os.remove(file)
+		# logger.debug(f"Mail was to be sent with:\n\t{logs}") # We don't wanna fill everyone's inbox with junk
+		# mailer.send()
